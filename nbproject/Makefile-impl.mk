f30e79e939bea910f118ae41b76eaeae3780ebdc
TehOcean <TehOcean@users.noreply.github.com>
2019-11-24 16:51:37 +0000

    tab name

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=SerialReaderTest.X
+PROJECTNAME=ECM
 
 # Active Configuration
 DEFAULTCONF=default
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=SerialReaderTest.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/SerialReaderTest.X.production.hex
+CND_ARTIFACT_NAME_default=ECM.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ECM.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=serialreadertest.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/serialreadertest.x.tar
+CND_PACKAGE_NAME_default=ecm.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ecm.tar
