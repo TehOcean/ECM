4cc4c8c9860e772bc8f03d5405a13609fdf118b6
Ossian Keith <TehOcean@users.noreply.github.com>
2019-11-27 18:10:18 +0000

    added new  led functions

---
+++ led.c
@@ -0,0 +1,15 @@
+/*
+ * File:   led.c
+ * Author: ossia
+ *
+ * Created on 27 November 2019, 18:04
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
